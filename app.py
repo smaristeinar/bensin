2c8d1eedeea7fbb4aa3903e95e95d1b0d0208f86
smari steinar <smarikul@hotmail.com>
2019-10-18 09:15:17 +0000

    finsished

--- app.py
+++ app.py
@@ -1,19 +1,70 @@
 from flask import Flask, render_template
 import os
+#idata["results"]
+from datetime import datetime
+import urllib.request, json
+from jinja2 import ext
+
+with urllib.request.urlopen("https://apis.is/petrol") as url:
+    data = json.loads(url.read().decode())
+
+stodvar = []
+done = []
+for i in data["results"]:
+    if i["company"] not in done:
+        done.append(i["company"])
+        stodvar.append([i["company"],i["company"].lower()+".png"])
+
+def getlowestprice(key,nafn):
+    low = 10000000000000000000
+    company = ""
+    name = ""
+    nafn = nafn
+    for i in data["results"]:
+        if i[key] < low:
+            company = i["company"]
+            name = i["name"]
+            low = i[key]
+
+    return f"Ódýrasta {nafn}: {low}kr. er hjá {company}, {name}"
+
+print(getlowestprice("diesel","dísel"))
+
 
 
 app = Flask(__name__)
 
+def format_time(data):
+    return datetime.strptime(data, "%Y-%m-%dT%H:%M:%S.%f").strftime("%d. %m. %Y Kl. %H: %M")
+
+print(format_time(data["timestampPriceCheck"]))
+app.jinja_env.filters["format_time"] = format_time
+
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html",data = data, stodvar = stodvar, disel = getlowestprice("diesel","dísel"), bensin = getlowestprice("bensin95","bensín"), )
+
+
+@app.route("/company/<name>")
+def company(name):
+    print(name)
+    for items in data["results"]:
+        if items["company"] == name:
+            print(items["geo"]["lat"])
+    return render_template("company.html", data = data, name = name)
+
+@app.route("/moreinfo/<name>")
+def moreinfo(name):
+    print("kallli")
+    return render_template("moreinfo.html", data = data , name = name)
 
 
 
-@app.errorhandler(404)
-def page_not_found():
+#@app.errorhandler(404)
+#def page_not_found():
     # note that we set the 404 status explicitly
-    return "síða fanst ekki 404"
+    #return "síða fanst ekki 404"
 
 
 
